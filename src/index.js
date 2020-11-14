56edbaf3e5da1ca3b178bd1ff1c35f84eaa32786
Ricardo Dias <cadu.hd@gmail.com>
2020-11-14 11:52:51 -0300

    add header component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { StatusBar } from 'react-native'
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
-const App = () => <Routes />;
+//<> - fragment- encapsula mais de um componente dentro dele
+// StatusBar - arruma a abarra superior só no android
+const App = () =>  (
+    <>
+        <StatusBar barStyle="light-content" backgroundColor='#8B10AE'/>
+        <Routes />
+    </>
+)
 
 export default App;
